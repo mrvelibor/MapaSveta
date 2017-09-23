514115d4b926d816d27ea8d07550b21fd1df01ef
Velibor Bacujkov <mr.velibor@gmail.com>
2017-09-23 15:11:06 +0200

    Google Map

--- MapaSveta-Frontend/src/app/app.component.ts
+++ MapaSveta-Frontend/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   url;
+  map;
 
   constructor() {
     this.url = environment.apiUrl;
+  }
+
+  ngOnInit() {
+    var props = {
+      center: new google.maps.LatLng(51.508742, -0.120850),
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(document.getElementById('map'), props);
   }
 }
