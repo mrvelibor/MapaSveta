ac809960ac23710cb13380523c82805965d07802
Velibor Bacujkov <mr.velibor@gmail.com>
2017-09-23 14:09:49 +0200

    Initial commit

---
+++ MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/MapasvetaApplication.java
@@ -0,0 +1,12 @@
+package com.mrvelibor.mapasveta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapasvetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapasvetaApplication.class, args);
+	}
+}
