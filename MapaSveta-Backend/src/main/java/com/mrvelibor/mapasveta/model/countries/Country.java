a725b905b81ff44daf21275489b76919e784a2f5
Velibor <mr.velibor@gmail.com>
2017-12-07 23:08:45 +0100

    Country Start

--- MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/model/countries/Country.java
+++ MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/model/countries/Country.java
@@ -2,6 +2,7 @@ package com.mrvelibor.mapasveta.model.countries;
 
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Country {
@@ -9,53 +10,25 @@ public class Country {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(unique = true)
-    private String countryCode;
+    private String commonName;
 
-    private String name;
+    private String officialName;
+
+    @Column(unique = true)
+    private String countryCode2;
+
+    @Column(unique = true)
+    private String countryCode3;
 
     private String diallingCode;
 
+    private String domain;
+
+    @OneToOne
+    private City capital;
+
     private String wikipediaUrl;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDiallingCode() {
-        return diallingCode;
-    }
-
-    public void setDiallingCode(String diallingCode) {
-        this.diallingCode = diallingCode;
-    }
-
-    public String getWikipediaUrl() {
-        return wikipediaUrl;
-    }
-
-    public void setWikipediaUrl(String wikipediaUrl) {
-        this.wikipediaUrl = wikipediaUrl;
-    }
+    private List<String> currencies;
 
 }
