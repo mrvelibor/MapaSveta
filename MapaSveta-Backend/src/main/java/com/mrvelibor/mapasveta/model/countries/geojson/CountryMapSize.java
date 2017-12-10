b8b0162dc2a06ed29d3aaf1644195e01869aa8d5
Velibor <mr.velibor@gmail.com>
2017-12-10 11:09:56 +0100

    Size10

--- MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/model/countries/geojson/CountryMapSize.java
+++ MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/model/countries/geojson/CountryMapSize.java
@@ -1,5 +1,5 @@
 package com.mrvelibor.mapasveta.model.countries.geojson;
 
 public enum CountryMapSize {
-    original, size_2, size_3, size_4, size_5
+    original, size_2, size_3, size_4, size_5, size_10
 }
