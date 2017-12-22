b1457dd7cde632efd3ff3f12397f7b49a99f69e8
Velibor Bacujkov <mr.velibor@gmail.com>
2017-12-22 14:39:55 +0100

    DAOs

--- MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/dao/CurrencyDao.java
+++ MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/dao/CurrencyDao.java
@@ -1,10 +1,9 @@
 package com.mrvelibor.mapasveta.dao;
 
-import com.mrvelibor.mapasveta.model.user.User;
+import com.mrvelibor.mapasveta.model.common.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+public interface CurrencyDao extends JpaRepository<Currency, Long> {
 }
---
+++ MapaSveta-Backend/src/main/java/com/mrvelibor/mapasveta/dao/RecommendationRatingDao.java
@@ -0,0 +1,13 @@
+package com.mrvelibor.mapasveta.dao;
+
+import com.mrvelibor.mapasveta.model.recommendations.RecommendationRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecommendationRatingDao extends JpaRepository<RecommendationRating, Long> {
+    List<RecommendationRating> findAllByUser_Id(Long userId);
+    List<RecommendationRating> findAllByRecommendation_Id(Long recommendationId);
+}
